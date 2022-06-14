ee89d69e20f76c4d35c4c5edf190e8da63898b21
ryuuki <c0a2111229@edu.teu.ac.jp>
2022-06-14 15:36:57 +0900

    サザエさんクイズ

---
+++ ex01/quiz.py
@@ -0,0 +1,27 @@
+import random
+import datetime
+a=random.randint(0,2)
+def shutudai(a):
+    monndai=["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係"]
+    print("問題：")
+    print(monndai[a])
+
+def kaito(a):
+    time1=datetime.now()
+    seikai=[["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
+    b=input("答えるんだ：")
+    if a==0 or a==1:
+        if b==seikai[a][0] or b==seikai[a][1]:
+            print("正解！！！")
+        else:
+            print("出直してこい")
+    else:
+        if b==seikai[a][0] or b==seikai[a][1] or b==seikai[a][2] or b==seikai[a][3]:
+            print("正解！！！")
+        else:
+            print("出直してこい")
+    time2=datetime.now()
+    print(f"所要時間：{time2-time1}")
+shutudai(a)
+kaito(a)
+
