f498110bcc0ee14fd9b8d4d02d6e4764fe14d2db
ryuuki <c0a2111229@edu.teu.ac.jp>
2022-06-21 14:57:55 +0900

    問6

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,21 +1,23 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def button_click(event):
+def button_click(event): #通知用の関数
     btn=event.widget
     txt=btn["text"]
-    tkm.showinfo(txt,f"{txt}のボタンが押されました")
+    #tkm.showinfo(txt,f"{txt}のボタンが押されました")
+    entry.insert(tk.END,txt)
 
 if __name__=='__main__':
     root=tk.Tk()
     root.title("電卓") #タイトル
-    root.geometry("300x500") #500x300の大きさのウィジェットを生成
-    r=0
+    entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
+    entry.grid(row=0,column=0,columnspan=3)
+    r=1
     c=0
     num=0
-    for i in [9,8,7,6,5,4,3,2,1,0]: #ボタンの作成
+    for i in [9,8,7,6,5,4,3,2,1,0,"+","="]: #ボタンの作成
         button=tk.Button(root,text=i,font=("Times New Roman",30)
-                        ,width=4,height=2,command=button_click)
+                        ,width=4,height=2)
         if num%3==0:
             r+=1
             c=0
