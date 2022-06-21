f6326002133ea681bff52f7427ef03b412828a94
ryuuki <c0a2111229@edu.teu.ac.jp>
2022-06-21 17:24:05 +0900

    直したね～　#8

--- ex02/calc.py
+++ ex02/calc.py
@@ -23,11 +23,11 @@ if __name__=='__main__':
     r=1
     c=0
     num=0
-    for i in [9,8,7,"*",6,5,4,"/",3,2,1,"-",0,"π","=","+","sin","cos","tan",".","ac"]: #ボタンの作成
+    for i in [9,8,7,"*","/",6,5,4,"+","-",3,2,1,".","ac",0,"π","=","sin","cos","tan",]: #ボタンの作成
         button=tk.Button(root,text=i,
                         font=("Times New Roman",30),
                         width=4,height=2)
-        if num%4==0:
+        if num%5==0:
             r+=1
             c=0
         button.grid(row=r,column=c)
