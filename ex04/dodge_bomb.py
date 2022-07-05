52c27864ac2c9f09b57675f06e816e53db52a69f
ryuuki <c0a2111229@edu.teu.ac.jp>
2022-07-05 16:46:58 +0900

    四限追加機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,8 +1,10 @@
 import pygame as pg
 import sys
 import random
+import time
 
 def main():
+    start=time.time() #ゲームが始まった時間
     #練習1
     clock=pg.time.Clock()
     pg.display.set_caption("逃げろ！こうかとん")
@@ -23,7 +25,17 @@ def main():
     bmimg_rct=bmimg_sfc.get_rect() #rect
     bmimg_rct.centerx=random.randint(0,screen_rct.width)
     bmimg_rct.centery=random.randint(0,screen_rct.height)
+    #新しい爆弾の追加
+    i=0
+    bmimg1_sfc=pg.Surface((20,20)) #Surface
+    bmimg1_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bmimg1_sfc,(0,0,255),(10,10),10)
+    bmimg1_rct=bmimg1_sfc.get_rect() #rect
+    bmimg1_rct.centerx=random.randint(0,screen_rct.width)
+    bmimg1_rct.centery=random.randint(0,screen_rct.height)
+
     vx, vy=+1, +1 #練習6
+    wx, wy=+1, +1 #二つ目の爆弾の移動ベクトル
 
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
@@ -43,13 +55,30 @@ def main():
             if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -=1
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
         bmimg_rct.move_ip(vx,vy) #練習6
+        bmimg1_rct.move_ip(wx,wy)
         screen_sfc.blit(bmimg_sfc,bmimg_rct) #練習5
+        if key_states[pg.K_2]==True: i=1
+        if i==1: screen_sfc.blit(bmimg1_sfc,bmimg1_rct)
         #練習7
         yoko, tate=check_bound(bmimg_rct,screen_rct)
         vx*=yoko
         vy*=tate
+        yoko, tate=check_bound(bmimg1_rct,screen_rct)
+        wx*=yoko
+        wy*=tate
         #練習8
-        if kkimg_rct.colliderect(bmimg_rct): return
+        #こうかとんの画像変更、敗北表示、強制終了
+        if kkimg_rct.colliderect(bmimg_rct) or (kkimg_rct.colliderect(bmimg1_rct) and i==1):
+            end=time.time() #ゲームが終わった時間
+            kkimg_sfc=pg.image.load("fig/8.png") #Surface
+            kkimg_sfc=pg.transform.rotozoom(kkimg_sfc,0,2.0)
+            screen_sfc.blit(kkimg_sfc,kkimg_rct)
+            fonto=pg.font.Font(None,140) #フォントの決定
+            screen_sfc.blit(fonto.render(str("Game Over"),True,"BLACK"),(550,450)) #Game Overの表示
+            screen_sfc.blit(fonto.render(str(f"time:{round(end-start,2)}"),True,"BLACK"),(550,570)) #timeの表示
+            pg.display.update()
+            time.sleep(5) #強制終了
+            return
         pg.display.update()
         clock.tick(1000)
 
