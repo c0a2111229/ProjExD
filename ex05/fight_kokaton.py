cddfd02a7e2ab14b5b872c0389cae77605908aaf
ryuuki <c0a2111229@edu.teu.ac.jp>
2022-07-12 17:26:54 +0900

    四限追加機能

--- ex05/fight_kokaton.py
+++ ex05/fight_kokaton.py
@@ -2,7 +2,6 @@ import pygame as pg
 import sys
 import random
 
-
 class Screen:
     def __init__(self, title, wh, image):
         pg.display.set_caption(title)
@@ -45,6 +44,9 @@ class Bird:
             if key_states[pg.K_RIGHT]:
                 self.rct.centerx -= 1
         self.blit(scr)
+    
+    def attack(self):
+        return Shot(self)
 
 
 class Bomb:
@@ -71,25 +73,64 @@ class Bomb:
         self.blit(scr)
 
 
+class Shot:
+    def __init__(self, chr: Bird):
+        self.sfc = pg.image.load("fig/beam.png")
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, 0.2)  # Surface
+        self.rct = self.sfc.get_rect()
+        self.rct.midleft = chr.rct.center    # Rect
+    
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr: Screen):
+        self.rct.move_ip(+10, 0) #右方向に速度１で移動する
+        self.blit(scr)
+        if check_bound(self.rct,scr.rct) != (1,1): #領域外に出たらインスタンスを消す
+            del self
+
+
 def main():
     clock = pg.time.Clock()
-    scr = Screen("逃げろ！こうかとん", (1600, 900), "fig/pg_bg.jpg")
+    scr = Screen("負けるな！こうかとん", (1600, 900), "fig/pg_bg.jpg")
     kkt = Bird("fig/6.png", 2.0, (900, 400))
-    bkd=Bomb((255, 0, 0), 10, (+1, +1), scr)
+    beams = []
+    bombs = []
+    brs={}
+    bombs.append(Bomb((255, 0, 0), 10, (+1, +1), scr))
+    brs[bombs[0]] = True
+    count=1
 
     while True:
         scr.blit()
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            if event.type == pg.QUIT:
+                return
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                beams.append(kkt.attack()) #スペースキーが押されたらこうかとんがビームを打つ
+                kkt = Bird("fig/9.png", 2.0, (900, 400))
+            if event.type == pg.KEYDOWN and event.key == pg.K_1:
+                bombs.append(Bomb((255, 0, 0), 10, (+1, +1), scr)) #1が押されたら爆弾が増える
+                brs[bombs[count]] = True
+                count+=1
 
         kkt.update(scr)
-        bkd.update(scr)
-        if kkt.rct.colliderect(bkd.rct):
-            return
+        for i in range(len(bombs)):
+            if brs[bombs[i]]:
+                bombs[i].update(scr)
+        if len(beams) != 0:
+            for beam in beams:
+                beam.update(scr)
+        for i in range(len(bombs)): #どの爆弾とぶつかっても終了する
+            for j in range(len(beams)):
+                if beams[i].rct.colliderect(bombs[i].rct):
+                    brs[bombs[i]] = False
+            if kkt.rct.colliderect(bombs[i].rct):
+                return
         
         pg.display.update()
         clock.tick(1000)
-
+        
 
 def check_bound(rct, scr_rct):
     '''
@@ -100,7 +141,6 @@ def check_bound(rct, scr_rct):
     if rct.left < scr_rct.left or scr_rct.right  < rct.right : yoko = -1 # 領域外
     if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom: tate = -1 # 領域外
     return yoko, tate
-
 
 
 if __name__ == "__main__":
